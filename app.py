9c44b8ed301ad0185dd6ddfa9fed16d183562849
nikitkah1love <nktprkcs@gmail.com>
2024-08-01 17:28:42 +0300

    Login and playin round

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from markupsafe import escape
+from flask import request
 
 app = Flask(__name__, template_folder='templates')
+
+
+def do_the_login():
+    pass
+
+def show_the_login_form():
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
 @app.route('/')
 def home():
